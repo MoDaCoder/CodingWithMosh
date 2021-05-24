f6681e9bb0ebe7b5989fce2984fe636f51776764
Guillermo Martinez <53015834+MoDaCoder@users.noreply.github.com>
2021-05-24 16:59:27 -0500

    Practicing simplifying if statements in Java.

---
+++ CodingWithMosh/src/codingwithmosh/SimplifyingIfStatements.java
@@ -0,0 +1,8 @@
+package codingwithmosh;
+
+public class SimplifyingIfStatements {
+    public static void main(String[] args){
+        int income = 120000;
+        boolean hasHighIncome = (income > 100000);
+    }
+}
