3c3cdd6a23cb7215b5aef77d85a1db33f92aa544
Guillermo Martinez <53015834+MoDaCoder@users.noreply.github.com>
2021-05-25 10:56:11 -0500

    Practicing while loops in Java.

--- CodingWithMosh/src/codingwithmosh/WhileLoops.java
+++ CodingWithMosh/src/codingwithmosh/WhileLoops.java
@@ -4,17 +4,18 @@ import java.util.Scanner;
 public class WhileLoops {
     
     public static void main(String[] args){
-//        int i = 0;
-//        while(i <= 5){
-//            System.out.println("Doing while loop!");
-//            i++;
-//        }
+        int i = 0;
+        while(i <= 5){
+            System.out.println("Doing while loop!");
+            i++;
+        }
         
         Scanner sc = new Scanner(System.in);
         String keyWord = "";
-        while(!keyWord.equals("quit"))
+        while(!keyWord.equals("quit")){
             System.out.println("What would you like to do: ");
             keyWord = sc.next().toLowerCase();
-            System.out.println(keyWord);
+            System.out.print(keyWord);
+        }
     }
 }
