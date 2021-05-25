f35176463cafb50164976fb17e26e1aae31944a9
Guillermo Martinez <53015834+MoDaCoder@users.noreply.github.com>
2021-05-25 08:18:34 -0500

    Practicing TernaryOperatos in java.

---
+++ CodingWithMosh/src/codingwithmosh/TernaryOperator.java
@@ -0,0 +1,9 @@
+package codingwithmosh;
+
+public class TernaryOperator {
+    public static void main(String[] args){
+        int income = 120000;
+        String className = income <= 100000 ? "First" : "Second";
+        System.out.println(className);
+        }
+    }
